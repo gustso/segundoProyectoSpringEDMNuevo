899470eb324c451d1d2afe039b9b3453ca5fbdba
gustso <gustso@fi.unju.edu.ar>
2021-03-25 19:18:59 -0300

    Inicializacion del Proyecto Resuelta la suma

---
+++ src/main/java/ar/edu/unju/edm/controller/CalculadoraController.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.edm.controller;
+
+import ar.edu.unju.edm.model.Calculadora;
+
+public class CalculadoraController {
+	
+	public void resolverOperaciones() {
+		int num1;
+			int num2;
+			num1 = 45;
+			num2 = 20;
+			
+			Calculadora nuevaCalculadora = new Calculadora();
+			
+			nuevaCalculadora.setA(num1);
+			nuevaCalculadora.setB(num2);
+			
+			System.out.println("El resultado de la suma es: "+ nuevaCalculadora.sumar());
+			
+	}
+
+}

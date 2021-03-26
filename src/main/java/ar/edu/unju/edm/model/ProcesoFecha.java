f4d9e4fd22469772bec0aa31c40085222f5a0f8b
Mauri <Mauri@192.168.0.14>
2021-03-26 16:28:33 -0300

    Agregado de clase proceso fecha.

---
+++ src/main/java/ar/edu/unju/edm/model/ProcesoFecha.java
@@ -0,0 +1,45 @@
+package ar.edu.unju.edm.model;
+
+public class ProcesoFecha {
+	private int dia; 
+	private int mes; 
+	private int año;
+	
+	public ProcesoFecha() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProcesoFecha(int dia, int mes, int año) {
+		super();
+		this.dia = dia;
+		this.mes = mes;
+		this.año = año;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+	
+	
+
+}

df00d101ecba556a8710d3435d89eb2227dfad06
gustso <gustso@fi.unju.edu.ar>
2021-03-26 16:37:35 -0300

    Creada una instancia de la clase ProcesoFecha Colaborador: Gustavo

--- src/main/java/ar/edu/unju/edm/controller/ProcesoFechaController.java
+++ src/main/java/ar/edu/unju/edm/controller/ProcesoFechaController.java
@@ -1,6 +1,8 @@
 package ar.edu.unju.edm.controller;
 
+import ar.edu.unju.edm.model.ProcesoFecha;
+
 public class ProcesoFechaController {
 
-	
+	ProcesoFecha nuevaFecha = new ProcesoFecha(26,3,2021);
 }
